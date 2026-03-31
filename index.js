9baa36c5a47f253d70c25aa4b1603b674d6ce0f5
Chandan C <chandanc405060@gmail.com>
2026-03-31 09:03:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,37 +4,46 @@ import simpleGit from "simple-git";
 import random from "random";
 
 const path = "./data.json";
+const git = simpleGit();
 
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
+// 🔧 SET YOUR RANGE HERE
+const START_DATE = "01/01/2024"; // dd/mm/yyyy
+const END_DATE = "20/12/2024";   // dd/mm/yyyy
+
+const start = moment(START_DATE, "DD/MM/YYYY");
+const end = moment(END_DATE, "DD/MM/YYYY");
+
+// Generate random date between range
+const getRandomDate = () => {
+  const diff = end.diff(start, "days");
+  const randomDays = random.int(0, diff);
+
+  return start
+    .clone()
+    .add(randomDays, "days")
+    .add(random.int(0, 23), "hours")
+    .add(random.int(0, 59), "minutes")
     .format();
-
-  const data = {
-    date: date,
-  };
-
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
-  });
 };
 
+// Recursive commit function (your style kept)
 const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+  if (n === 0) return git.push();
+
+  const date = getRandomDate();
 
   const data = {
     date: date,
   };
+
   console.log(date);
+
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+    git
+      .add([path])
+      .commit(`commit ${n}`, { "--date": date }, makeCommits.bind(this, --n));
   });
 };
 
+// 🔥 CHANGE NUMBER OF COMMITS HERE
 makeCommits(100);
